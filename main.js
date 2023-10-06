280f8234e6c1c6b87ed496c89070fd7b86a541e8
tainarafernandes <125694417+tainarafernandes@users.noreply.github.com>
2023-10-06 17:20:34 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// Pom -----------------------------------------------
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
