70a5a9bbadd5983e1e6d4421b123cd4ecf568a59
tainarafernandes <125694417+tainarafernandes@users.noreply.github.com>
2023-10-20 16:51:43 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
-// Pom -----------------------------------------------
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template string
+    const idAudio = `#som_${instrumento}`;
+
+    //console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom('idAudio');
+    }
+
+    contador = contador + 1;
+
+    //console.log(contador);
+
+}
+
+
 
 
 
